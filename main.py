0b2809421abdcec814b6f64781550a45579a6ae6
Noah Kim <noahkim111@gmail.com>
2022-04-22 11:30:21 -0400

    reformat

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from friendApi import getWebAccessToken, getFriendActivity
+from dotenv import load_dotenv
+import threading
 import datetime
 import os
-from dotenv import load_dotenv
+
 
 load_dotenv()
 
 def main():
+    threading.Timer(300.0, main).start()
     token = os.getenv('SPOTIFY_TOKEN')
     accessToken = getWebAccessToken(token)
     friendActivity = getFriendActivity(accessToken)
@@ -14,7 +17,7 @@ def main():
     for (i, friend) in enumerate(friendActivity['friends']):
         print(f'{i+1}. {friend["user"]["name"]} at {(datetime.datetime.fromtimestamp(int(friend["timestamp"])/1000)).strftime("%Y-%m-%d %H:%M:%S")}')
         print(f'\t{friend["track"]["name"]} by {friend["track"]["artist"]["name"]}')
-        print(f'\tLink: open.spotify.com/track/{friend["track"]["uri"][14:]}')
+        print(f'\tLink: https://open.spotify.com/track/{friend["track"]["uri"][14:]}')
         print('\n')
 
 main()
